337fb2fd35fa1a7cce6cfcc0d5225b50dc6db814
manuyadav7351 <manuyadav7351@gmail.com>
2026-01-28 16:18:08 +0530

    fixed

--- time-slot-manager/src/components/TimeZoneDrop.tsx
+++ time-slot-manager/src/components/TimeZoneDrop.tsx
@@ -32,7 +32,7 @@ const TimezoneDropdown: React.FC<TimezoneDropdownProps> = ({
         disabled={isLoading || !timezones.length}
         className="timezone-select"
       >
-        <option value="">-- Select a timezone --</option>
+        <option className="timezone-select-bg" value="">-- Select a timezone --</option>
         {timezones.map((tz) => (
           <option key={tz.id} value={tz.id}>
             {tz.name} ({tz.id})
